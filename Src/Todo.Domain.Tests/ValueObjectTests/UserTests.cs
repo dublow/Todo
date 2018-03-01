e48cad1b4f438a11136388f54d3833fea1a0dc3a
dublow <nicolas.dfr@gmail.com>
2018-03-01 16:51:05 +0100

    Added User ValueObject & tested

---
+++ Src/Todo.Domain.Tests/ValueObjectTests/UserTests.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Domain.ValueObjects;
+
+namespace Todo.Domain.Tests.ValueObjectTests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void ShouldBeNotEqualWhenUserIsNull()
+        {
+            // arrange
+            User user = null;
+            User otherUser = User.Create("nicolas");
+
+            // act
+            var actual = otherUser.Equals(user);
+
+            // assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualWhenUserHasSameReference()
+        {
+            // arrange
+            User user = User.Create("nicolas");
+            User otherUser = user;
+
+            // act
+            var actual = user.Equals(otherUser);
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualWhenUserHasSameValue()
+        {
+            // arrange
+            User user = User.Create("nicolas");
+            User otherUser = User.Create("nicolas");
+
+            // act
+            var actual = user.Equals(otherUser);
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenUserHasSameReference()
+        {
+            // arrange
+            User user = User.Create("nicolas");
+            User otherUser = user;
+
+            // act
+            var actual = user == otherUser;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenUserHasSameValue()
+        {
+            // arrange
+            User user = User.Create("nicolas");
+            User otherUser = User.Create("nicolas");
+
+            // act
+            var actual = user == otherUser;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenUserHasSameHashcode()
+        {
+            // arrange
+            User user = User.Create("nicolas");
+            User otherUser = User.Create("nicolas");
+
+            // act
+            var actual = user.GetHashCode() == otherUser.GetHashCode();
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeNotEqualByOperatorWhenUserHasDifferentValue()
+        {
+            // arrange
+            User user = User.Create("nicolas");
+            User otherUser = User.Create("jean");
+
+            // act
+            var actual = user != otherUser;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldThrownArgumentNullExceptionWhenNameIsEmpty()
+        {
+            // assert
+            Assert.ThrowsException<ArgumentNullException>(() => User.Create(string.Empty));
+        }
+
+        [TestMethod]
+        public void ShouldCapitalizeName()
+        {
+            // arrange
+            var name = "NICOLAS";
+
+            // act
+            var actual = User.Create(name);
+
+            // assert
+            Assert.AreEqual("Nicolas", actual.Name);
+        }
+    }
+}
---
+++ Src/Todo.Domain/ValueObjects/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Todo.Domain.ValueObjects
+{
+    public sealed class User : ValueObject<User>
+    {
+        public static User Create(string name) =>
+            new User(name);
+
+        public string Name { get; }
+
+        private User(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+
+            Name = CapitalizeName(name);
+        }
+
+        public override bool EqualsCore(User other)
+        {
+            return Name == other.Name;
+        }
+
+        public override int GetHashCodeCore()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        private string CapitalizeName(string value)
+        {
+            return new String(value.Select((c, index) => {
+                return index == 0
+                    ? Char.ToUpper(c)
+                    : Char.ToLower(c);
+            }).ToArray());
+        }
+    }
+}
