354b40b4f23214df1587385b5504881c73695db5
dublow <nicolas.dfr@gmail.com>
2018-03-01 16:59:33 +0100

    Added Category ValueObject & tested

---
+++ Src/Todo.Domain.Tests/ValueObjectTests/CategoryTests.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.ValueObjects;
+
+namespace Todo.Domain.Tests.ValueObjectTests
+{
+    [TestClass]
+    public class CategoryTests
+    {
+        [TestMethod]
+        public void ShouldBeNotEqualWhenCategoryIsNull()
+        {
+            // arrange
+            Category Category = null;
+            Category otherCategory = Category.Create("nicolas");
+
+            // act
+            var actual = otherCategory.Equals(Category);
+
+            // assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualWhenCategoryHasSameReference()
+        {
+            // arrange
+            Category Category = Category.Create("nicolas");
+            Category otherCategory = Category;
+
+            // act
+            var actual = Category.Equals(otherCategory);
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualWhenCategoryHasSameValue()
+        {
+            // arrange
+            Category Category = Category.Create("nicolas");
+            Category otherCategory = Category.Create("nicolas");
+
+            // act
+            var actual = Category.Equals(otherCategory);
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenCategoryHasSameReference()
+        {
+            // arrange
+            Category Category = Category.Create("nicolas");
+            Category otherCategory = Category;
+
+            // act
+            var actual = Category == otherCategory;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenCategoryHasSameValue()
+        {
+            // arrange
+            Category Category = Category.Create("nicolas");
+            Category otherCategory = Category.Create("nicolas");
+
+            // act
+            var actual = Category == otherCategory;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenCategoryHasSameHashcode()
+        {
+            // arrange
+            Category Category = Category.Create("nicolas");
+            Category otherCategory = Category.Create("nicolas");
+
+            // act
+            var actual = Category.GetHashCode() == otherCategory.GetHashCode();
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeNotEqualByOperatorWhenCategoryHasDifferentValue()
+        {
+            // arrange
+            Category Category = Category.Create("nicolas");
+            Category otherCategory = Category.Create("jean");
+
+            // act
+            var actual = Category != otherCategory;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldThrownArgumentNullExceptionWhenNameIsEmpty()
+        {
+            // assert
+            Assert.ThrowsException<ArgumentNullException>(() => Category.Create(string.Empty));
+        }
+
+        [TestMethod]
+        public void ShouldCapitalizeName()
+        {
+            // arrange
+            var name = "NICOLAS";
+
+            // act
+            var actual = Category.Create(name);
+
+            // assert
+            Assert.AreEqual("Nicolas", actual.Name);
+        }
+    }
+}
---
+++ Src/Todo.Domain/ValueObjects/Category.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Todo.Domain.ValueObjects
+{
+    public sealed class Category : ValueObject<Category>
+    {
+        public static Category Create(string name) =>
+            new Category(name);
+
+        public string Name { get; }
+
+        private Category(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+
+            Name = CapitalizeName(name);
+        }
+
+        public override bool EqualsCore(Category other)
+        {
+            return Name == other.Name;
+        }
+
+        public override int GetHashCodeCore()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        private string CapitalizeName(string value)
+        {
+            return new String(value.Select((c, index) => {
+                return index == 0
+                    ? Char.ToUpper(c)
+                    : Char.ToLower(c);
+            }).ToArray());
+        }
+    }
+}
