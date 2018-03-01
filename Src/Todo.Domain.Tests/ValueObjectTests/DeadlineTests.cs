1ab0b1451f10dcadec40aba1bd7d72d2b71d49a1
dublow <nicolas.dfr@gmail.com>
2018-03-01 13:49:07 +0100

    Test Deadline equality

---
+++ Src/Todo.Domain.Tests/ValueObjectTests/DeadlineTests.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.ValueObjects;
+
+namespace Todo.Domain.Tests.ValueObjectTests
+{
+    [TestClass]
+    public class DeadlineTests
+    {
+        [TestMethod]
+        public void ShouldBeNotEqualWhenDeadlineIsNull()
+        {
+            // arrange
+            Deadline deadline = null;
+            Deadline otherDeadline = new Deadline(DateTime.MinValue);
+
+            // act
+            var actual = otherDeadline.Equals(deadline);
+
+            // assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualWhenDeadlineHasSameReference()
+        {
+            // arrange
+            Deadline deadline = new Deadline(DateTime.MinValue);
+            Deadline otherDeadline = deadline;
+
+            // act
+            var actual = deadline.Equals(otherDeadline);
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualWhenDeadlineHasSameValue()
+        {
+            // arrange
+            Deadline deadline = new Deadline(DateTime.MinValue);
+            Deadline otherDeadline = new Deadline(DateTime.MinValue);
+
+            // act
+            var actual = deadline.Equals(otherDeadline);
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenDeadlineHasSameValue()
+        {
+            // arrange
+            Deadline deadline = new Deadline(DateTime.MinValue);
+            Deadline otherDeadline = new Deadline(DateTime.MinValue);
+
+            // act
+            var actual = deadline == otherDeadline;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenDeadlineHasSameReference()
+        {
+            // arrange
+            Deadline deadline = new Deadline(DateTime.MinValue);
+            Deadline otherDeadline = deadline;
+
+            // act
+            var actual = deadline == otherDeadline;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualByOperatorWhenDeadlineHasSameHashcode()
+        {
+            // arrange
+            Deadline deadline = new Deadline(DateTime.MinValue);
+            Deadline otherDeadline = deadline;
+
+            // act
+            var actual = deadline.GetHashCode() == otherDeadline.GetHashCode();
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void ShouldBeNotEqualByOperatorWhenDeadlineHasDifferentValue()
+        {
+            // arrange
+            Deadline deadline = new Deadline(DateTime.MinValue);
+            Deadline otherDeadline = new Deadline(DateTime.MaxValue);
+
+            // act
+            var actual = deadline != otherDeadline;
+
+            // assert
+            Assert.IsTrue(actual);
+        }
+    }
+}
---
+++ Src/Todo.Domain/ValueObjects/Deadline.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Todo.Domain.ValueObjects
+{
+    public sealed class Deadline : ValueObject<Deadline>
+    {
+        private readonly DateTime _value;
+
+        public Deadline(DateTime value)
+        {
+            _value = value;
+        }
+
+        public override bool EqualsCore(Deadline other)
+        {
+            return _value == other._value;
+        }
+
+        public override int GetHashCodeCore()
+        {
+            return _value.GetHashCode();
+        }
+    }
+}
